c7d038b4f079516365d1883c894c9119c9b423ce
701ka <muslimakarimova73@gmail.com>
2026-05-04 13:54:22 +0500

    fix: production db path and cors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,18 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "https://packs-lovat.vercel.app",
+      "http://localhost:4000",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  }),
+);
+
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 
@@ -13,6 +24,5 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/packs", require("./routes/packs"));
 app.use("/api/admin", require("./routes/admin"));
 
-// SHU QATORNI O'ZGARTIRING:
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
