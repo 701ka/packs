6805d01a1269d74e6ec64e7a86cc79840292ab36
701ka <muslimakarimova73@gmail.com>
2026-05-04 14:17:35 +0500

    feat: localStorage + AI description generator

--- server/db.js
+++
@@ -1,9 +0,0 @@
-const Database = require("better-sqlite3");
-const path = require("path");
-
-const dbPath =
-  process.env.NODE_ENV === "production"
-    ? "/data/editorpack.db"
-    : "editorpack.db";
-
-const db = new Database(dbPath);
