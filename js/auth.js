601129abb676083d79d0d186952fabb1135ca5b3
701ka <muslimakarimova73@gmail.com>
2026-05-07 23:40:53 +0500

    Clear old localStorage pack data

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,7 @@
+["ep_packs", "ep_users", "ep_user", "ep_redirect", "ep_subscribers"].forEach(
+  (key) => localStorage.removeItem(key),
+);
+
 function getStoredUser() {
   return JSON.parse(localStorage.getItem("user") || "null");
 }
